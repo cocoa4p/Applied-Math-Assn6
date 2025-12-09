ec486050299b422f0433f9a4b175e8e1f1a4c332
Anagha <118123726+anaghabbu@users.noreply.github.com>
2025-12-09 18:46:12 -0500

    fixed follower

--- string_traveling_wave_sim.m
+++ string_traveling_wave_sim.m
@@ -4,7 +4,7 @@ function string_traveling_wave_sim()
     total_mass = 5;
     tension_force = 5;
     string_length = 10;
-    damping_coeff = 0;  
+    damping_coeff = 0.0001;  
 
     dx = string_length/(num_masses+1);
 
@@ -15,12 +15,12 @@ function string_traveling_wave_sim()
     xlist = linspace(0, string_length, num_masses+2);  
 
     % Triangle pulse parameters
-    pulse_width = 1.0;
+    pulse_width = string_length/4;
     pulse_height = 1.0;
 
     % Right-end pulse function and derivative using your triangle_pulse functions
-    Uf_func = @(t) triangle_pulse(t, pulse_width, pulse_height);
-    dUfdt_func = @(t) triangle_pulse_derivative(t, pulse_width, pulse_height);
+    Uf_func = @(t) triangle_pulse(t, pulse_width/c, pulse_height);
+    dUfdt_func = @(t) triangle_pulse_derivative(t, pulse_width/c, pulse_height);
 
     % Store string parameters
     string_params = struct();
@@ -52,10 +52,10 @@ function string_traveling_wave_sim()
     figure; hold on;
 
     hString = plot(xlist, zeros(size(xlist)), 'k-', 'LineWidth', 2);
-    hMarker = plot(0, 0, 'ro', 'MarkerSize', 10, 'LineWidth', 2); 
+    hMarker = plot(0, 0, 'r', 'MarkerSize', 10, 'LineWidth', 2); 
 
-    ylim([-1.2, 1.2]);
-    xlim([0 string_length]);
+    ylim([-5, 5]);
+    xlim([-5 string_length]);
     xlabel('x');
     ylabel('displacement u(x,t)');
     title('Traveling Wave on a Discretized String (Triangle Pulse)');
@@ -80,10 +80,18 @@ function string_traveling_wave_sim()
         set(hString, 'YData', y_now);
 
         % move marker at speed c (just a visualization reference)
-        marker_x = string_length - c*(tlist(k) - 1);
-        if marker_x >= 0 && marker_x <= string_length
-            set(hMarker, 'XData', marker_x, 'YData', 0);
+        marker_x = string_length - c*tlist(k) + pulse_width/2;
+        %short blurb showing how to find x-coord of tracking line
+        %x = x-coord of tracking line, t = current time
+        %c = wave speed, w = pulse width (in time), L = string length
+        marker_x = mod(marker_x,2*string_length);
+        if marker_x > string_length
+        marker_x = 2*string_length - marker_x;
         end
+        set(hMarker, 'XData', [marker_x,marker_x], 'YData', pulse_height*[-1,1]);
+        % if marker_x >= 0 && marker_x <= string_length
+            % set(hMarker, 'XData', marker_x, 'YData', 0);
+        % end
 
         drawnow;
     end
